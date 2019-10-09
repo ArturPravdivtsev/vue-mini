8750e13915e7805c289d77bf3804362739b36d2f
ArturPravdivtsev <33483323+ArturPravdivtsev@users.noreply.github.com>
2019-10-09 08:46:17 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+const car = (name, model, owner, year, phone, image) =>({name, model, owner, year, phone, image})
+
+const cars = [
+    car("Ford", "Focus", "Max", 2016, "+7 929 123 45 67", "images/focus.jpg"),
+    car("Ford", "Mondeo", "Vladimir", 2018, "+7 900 523 46 67", "images/mondeo.jpg"),
+    car("Porshe", "Panamera", "Irina", 2015, "+7 609 123 45 00", "images/panamera.jpg"),
+]
+
+new Vue({
+    el: '#app',
+    data: {
+        cars: cars,
+        car: cars[0],
+        selectedCarIndex: 0,
+        phoneVisibility: false,
+    },
+    methods: {
+        selectCar: function(index) {
+            this.car = cars[index];
+            this.selectedCarIndex = index;
+        }
+    }
+})
